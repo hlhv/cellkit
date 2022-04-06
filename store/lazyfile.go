9f4a56207de9c7214280082dee815e26db5c03a6
Sasha Koshka <supergriffin64@gmail.com>
2022-04-06 02:50:21 -0400

    Added resource server

---
+++ store/lazyfile.go
@@ -0,0 +1,110 @@
+package store
+
+import (
+        "os"
+        "io"
+        "strings"
+        "net/http"
+        "path/filepath"
+        "github.com/hlhv/protocol"
+        "github.com/hlhv/cellkit/client"
+)
+
+/* chunkSize does not refer to actual chunked encoding. This is just so the
+ * client doesn't have to wait for the cell to send everything over and the
+ * queen to send that over before recieving everything. It should be at least
+ * 512 in order for accurate mime-type detection.
+ */
+const chunkSize int = 1024
+
+/* LazyFile is a struct capable of serving a file. The file is cached into
+ * memory when it is first loaded, hence the name.
+ */
+type LazyFile struct {
+        FilePath string
+        mime     string
+        chunks   []fileChunk
+}
+
+type fileChunk []byte
+
+/* Send sends the file along with a content-type header.
+ */
+func (item *LazyFile) Send (
+        band *client.Band,
+        head *protocol.FrameHTTPReqHead,
+) (
+        err error,
+) {
+        if item.chunks == nil {
+                err = item.loadAndSend(band, head)
+                return err
+        }
+        
+        _, err = band.WriteHTTPHead(200, map[string] []string{
+                "content-type": []string { item.mime },
+        })
+        if err != nil { return err }
+
+        for _, chunk := range(item.chunks) {
+                _, err = band.WriteHTTPBody(chunk)
+                if err != nil { return err }
+        }
+
+        return nil
+}
+
+/* loadAndSend loads the file from disk while sending it in response to an http
+ * request. This should be called when there is an http request for this file
+ * but it has not been loaded yet.
+ */
+func (item *LazyFile) loadAndSend (
+        band *client.Band,
+        head *protocol.FrameHTTPReqHead,
+) (
+        err error,
+) {
+        file, err := os.Open(item.FilePath)
+        defer file.Close()
+        if err != nil { return err }
+
+        needMime := true
+        for {
+                chunk := make([]byte, chunkSize)
+                bytesRead, err := io.ReadFull(file, chunk)
+                chunk = chunk[:bytesRead]
+		if err != nil && err != io.ErrUnexpectedEOF { return err }
+
+                if needMime {
+                        needMime = false
+                        item.mime = mimeSniff(item.FilePath, chunk)
+                        _, err = band.WriteHTTPHead(200, map[string] []string{
+                                "content-type": []string { item.mime },
+                        })
+                        if err != nil { return err }
+                }
+
+                item.chunks = append(item.chunks, chunk)
+                band.WriteHTTPBody(chunk)
+		
+                if err == io.ErrUnexpectedEOF { break }
+        }
+        
+        return nil
+}
+
+/* mimeSniff determines the content type of a byte array and an associated name.
+ * This isn't very good as of now but it works!
+ */
+func mimeSniff (name string, data []byte) (mime string) {
+        extension := filepath.Ext(name)
+        mime = http.DetectContentType(data)
+        if (
+                strings.HasPrefix(mime, "text/plain") &&
+                extension != ".txt" &&
+                extension != "" ) {
+                
+                mime = strings.Replace(mime, "plain", extension[1:], 1)
+        }
+        return mime
+}
---
+++ store/store.go
@@ -0,0 +1,67 @@
+package store
+
+import (
+        "errors"
+        "github.com/hlhv/protocol"
+        "github.com/hlhv/cellkit/client"
+)
+
+/* Store is a simple resource manager for serving static file resources. Files
+ * can be registered and unregistered dynamically, and are loaded lazily. It can
+ * be combined with any other system for serving files and pages.
+ */
+type Store struct {
+        items map[string] *LazyFile
+}
+
+/* New creates a new Store.
+ */
+func New () (store *Store) {
+        return &Store {
+                items: make(map[string] *LazyFile),
+        }
+}
+
+/* Register registers a file located at the filepath on the specific url path.
+ * Url paths must start from /, and not end in /.
+ */
+func (store *Store) Register (filePath string, webPath string) (err error) {
+        if webPath[0] != '/' {
+                return errors.New("web path must start at /")
+        }
+        if webPath[len(webPath) - 1] == '/' {
+                return errors.New("web path must be a file, not a directory")
+        }
+        store.items[webPath] = &LazyFile { FilePath: filePath }
+        return nil
+}
+
+/* Unregister finds the file registered at the specified url path and
+ * unregisters it, freeing it from memory
+ */
+func (store *Store) Unregister (webPath string) (err error) {
+        _, exists := store.items[webPath]
+        if !exists {
+                return errors.New("path " + webPath + " is not registered")
+        }
+        delete(store.items, webPath)
+        return nil
+}
+
+/* TryHandle checks the request path against the map of registered files, and
+ * serves a match if it finds it. The function returns wether it served a file
+ * or not. If this function returns false, the request needs to be handled
+ * still.
+ */
+func (store *Store) TryHandle (
+        band *client.Band,
+        head *protocol.FrameHTTPReqHead,
+) (
+        handled bool,
+        err     error,
+) {
+        item, matched := store.items[head.Path]
+        if !matched { return false, nil }
+        err = item.Send(band, head)
+        return true, err
+}
